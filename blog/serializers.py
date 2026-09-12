9ba0dde84b0f0c3504d15d6eaf1c4d5e75d742d7
Science Prince <scienceprince123@gmail.com>
2026-09-12 09:18:07 +0100

    Remove SerializerMethodField to allow Course and Blog image uploads

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,4 +1,3 @@
-import cloudinary
 from rest_framework import serializers
 from django.utils.text import slugify
 from .models import BlogPost, BlogCategory
@@ -11,9 +10,7 @@ class BlogCategorySerializer(serializers.ModelSerializer):
 class BlogPostSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name', read_only=True)
     author_name = serializers.SerializerMethodField()
-    
-    # Use the exact same working method
-    featured_image = serializers.SerializerMethodField()
+    # REMOVED SerializerMethodField for featured_image so it can accept uploads
 
     class Meta:
         model = BlogPost
@@ -30,17 +27,6 @@ class BlogPostSerializer(serializers.ModelSerializer):
             if full_name:
                 return full_name
         return obj.author.username
-
-    def get_featured_image(self, obj):
-        if obj.featured_image:
-            url = str(obj.featured_image)
-            # If it's already a full URL, return it
-            if url.startswith('http'):
-                return url
-            # If it's a relative path, make it absolute
-            cloud_name = cloudinary.config().cloud_name or 'grpuqogx'
-            return f"https://res.cloudinary.com/{cloud_name}/{url}"
-        return None
 
     def create(self, validated_data):
         if 'slug' not in validated_data or not validated_data['slug']:
